3989f6f94949449b1ad85a1c496897cada6340db
Kolos Horvath <kolos.horvath@cheppers.com>
2019-07-04 09:20:46 +0200

    Add test to Utils class

---
+++ tests/src/Unit/UtilsTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Cheppers\OtpspClient\Tests\Unit;
+
+use Cheppers\OtpspClient\Utils;
+use PHPUnit\Framework\TestCase;
+
+class UtilsTest extends TestCase
+{
+    public function casesFlatArray()
+    {
+        return [
+            'empty array' => [
+                [],
+                [],
+                []
+            ],
+            'one dimensional array, no skip' => [
+                [
+                    'bar',
+                    'bak',
+                ],
+                [
+                    'foo' => 'bar',
+                    'baz' => 'bak',
+                ],
+                [],
+            ],
+            'one dimensional array with skip' => [
+                [
+                    'bar',
+                ],
+                [
+                    'foo' => 'bar',
+                    'baz' => 'bak',
+                ],
+                [
+                    'baz'
+                ],
+            ],
+            'multi dimensional array, no skip' => [
+                [
+                    'one',
+                    'two',
+                    'three',
+                    'bak',
+                ],
+                [
+                    'foo' => [
+                        'one',
+                        'two',
+                        'three',
+                    ],
+                    'baz' => 'bak',
+                ],
+                [],
+            ],
+            'multi dimensional array with skip' => [
+                [
+                    'one',
+                    'two',
+                    'three',
+                ],
+                [
+                    'foo' => [
+                        'one',
+                        'two',
+                        'three',
+                    ],
+                    'baz' => 'bak',
+                ],
+                [
+                    'baz'
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider casesFlatArray
+     */
+    public function testFlatArray(array $expected, array $given, array $skip)
+    {
+        $actual = (new Utils())
+            ->flatArray($given, $skip);
+
+        static::assertSame($expected, $actual);
+    }
+}
