ccbf0101d6377f36c1afc20371ffea815d9d2bf9
David Olah <david.olah@cheppers.com>
2019-07-04 09:00:38 +0200

    unit and phpcs fix

--- tests/src/Unit/UrlParserTest.php
+++ tests/src/Unit/UrlParserTest.php
@@ -13,16 +13,19 @@ class UrlParserTest extends TestCase
     public function casesGetUrlQueryVariable()
     {
         return [
-            'bar',
-            'http://example.com?foo=bar&bar=foo',
-            'foo',
+            'key exists in url' => [
+                'bar',
+                'http://example.com?foo=bar&bar=foo',
+                'foo',
+            ],
         ];
     }
 
     /**
      * @dataProvider casesGetUrlQueryVariable
      */
-    public function testGetUrlQueryVariable(string $expected, string $url, string $key) {
+    public function testGetUrlQueryVariable(string $expected, string $url, string $key)
+    {
         $actual = (new UrlParser())->getUrlQueryVariable($url, $key);
 
         static::assertSame($expected, $actual);
