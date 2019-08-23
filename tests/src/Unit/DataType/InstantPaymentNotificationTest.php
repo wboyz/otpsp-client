7f70c5d4c318f7579341cf24b2c1d546de035f8a
Kolos Horvath <kolos.horvath@cheppers.com>
2019-08-23 16:55:29 +0200

    Issue #26563 - Refactor code and add example.php to the repository

---
+++ tests/src/Unit/DataType/InstantPaymentNotificationTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Cheppers\OtpspClient\Tests\Unit\DataType;
+
+use Cheppers\OtpspClient\DataType\InstantPaymentNotification;
+use PHPUnit\Framework\TestCase;
+
+class InstantPaymentNotificationTest extends ResponseBaseTestBase
+{
+    /**
+     * @var string|\Cheppers\OtpspClient\DataType\RequestBase
+     */
+    protected $className = InstantPaymentNotification::class;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function casesSetState(): array
+    {
+        return [
+            'empty' => [new InstantPaymentNotification(), []],
+            'basic' => [
+                $this->getBaseIpn(),
+                [
+                    'salt'          => 'test-salt',
+                    'orderRef'      => 'test-order-ref',
+                    'method'        => 'CARD',
+                    'merchant'      => 'test-merchant',
+                    'finishDate'    => '2019-09-01T00:12:42+02:00',
+                    'paymentDate'   => '2019-09-02T00:12:42+02:00',
+                    'transactionId' => 42,
+                    'status'        => 'FINISHED',
+                    'receiveDate'   => '2019-09-03T00:12:42+02:00',
+                ],
+            ],
+        ];
+    }
+
+    public function casesJsonSerialize()
+    {
+        return [
+            'empty' => [
+                [
+                    'method'        => 'CARD',
+                    'finishDate'    => '',
+                    'paymentDate'   => '',
+                    'status'        => '',
+                    'receiveDate'   => '',
+                    'salt'          => '',
+                    'merchant'      => '',
+                    'orderRef'      => '',
+                    'transactionId' => 0,
+                ],
+                new InstantPaymentNotification(),
+            ],
+            'basic' => [
+                [
+                    'method'        => 'CARD',
+                    'finishDate'    => '2019-09-01T00:12:42+02:00',
+                    'paymentDate'   => '2019-09-02T00:12:42+02:00',
+                    'status'        => 'FINISHED',
+                    'receiveDate'   => '2019-09-03T00:12:42+02:00',
+                    'salt'          => 'test-salt',
+                    'merchant'      => 'test-merchant',
+                    'orderRef'      => 'test-order-ref',
+                    'transactionId' => 42,
+                ],
+                $this->getBaseIpn(),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider casesJsonSerialize
+     */
+    public function testJsonSerialize(array $expected, InstantPaymentNotification $request)
+    {
+        static::assertSame($expected, $request->jsonSerialize());
+    }
+
+    protected function getBaseIpn(): InstantPaymentNotification
+    {
+        $expectedIpn = new InstantPaymentNotification();
+        $expectedIpn->salt = 'test-salt';
+        $expectedIpn->orderRef = 'test-order-ref';
+        $expectedIpn->method = 'CARD';
+        $expectedIpn->merchant = 'test-merchant';
+        $expectedIpn->finishDate = '2019-09-01T00:12:42+02:00';
+        $expectedIpn->paymentDate = '2019-09-02T00:12:42+02:00';
+        $expectedIpn->transactionId = 42;
+        $expectedIpn->status = 'FINISHED';
+        $expectedIpn->receiveDate = '2019-09-03T00:12:42+02:00';
+
+        return $expectedIpn;
+    }
+}
--- tests/src/Unit/DataType/ResponseBaseTestBase.php
+++ tests/src/Unit/DataType/ResponseBaseTestBase.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Cheppers\OtpspClient\Tests\Unit\DataType;
 
+use Cheppers\OtpspClient\DataType\ResponseBase;
 use PHPUnit\Framework\TestCase;
 
 abstract class ResponseBaseTestBase extends TestCase
@@ -19,22 +20,8 @@ abstract class ResponseBaseTestBase extends TestCase
     /**
      * @dataProvider casesSetState
      */
-    public function testSetState(array $expected, array $values): void
+    public function testSetState(ResponseBase $expected, array $values): void
     {
-        $data = $this->className::__set_state($values);
-        foreach ($expected as $key => $value) {
-            static::assertSame($value, $data->{$key});
-        }
-    }
-
-    abstract public function casesExportForChecksum(): array;
-
-    /**
-     * @dataProvider casesExportForChecksum
-     */
-    public function testExportForChecksum(array $expected, array $values): void
-    {
-        $data = $this->className::__set_state($values);
-        static::assertSame($expected, $data->exportForChecksum());
+        static::assertEquals($expected, $this->className::__set_state($values));
     }
 }
